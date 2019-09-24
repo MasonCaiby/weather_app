d57904473a2a7436cc1057d6c650ee511045b360
Mason <maxwell.caudle@gmail.com>
2019-09-24 09:04:48 -0600

    updated webapp and config file

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ cities = ['abcdef', 'testingtest']
 @app.route('/', methods=['GET','POST'])
 def my_form_post():
     text = request.form.get('text')
-
+    city = request.form.get('city')
+    print(text)
+    print(city)
     return render_template('add_user.html')
 
 if __name__ == "__main__":
